eb90580a91dc8b4a2b0c7ba6276783befe4f4827
Alessandro <sandrofico.dev@gmail.com>
2017-10-13 15:51:15 +0100

    delete route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,10 +21,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
-
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
@@ -33,6 +29,17 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was destroyed"
+    redirect_to articles_path
   end
 
   private
